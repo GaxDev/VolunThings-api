bbebc5a864f91e9459636980bc716497e5797ed9
MARCOS RUIZ GONZALEZ <ruizgonzalezmarcos04@gmail.com>
2026-03-18 13:34:00 +0100

    CREACION DE TABLAS

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { pool } from "../config/db";
 
 export const allUsers = async () => {
-    return await pool.query("SELECT * FROM users_dos");
+    return await pool.query("SELECT * FROM user_dos");
 }
