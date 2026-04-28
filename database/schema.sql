fdfc833c463d8cf1700f6abd87b7b7c94bbccfc7
MARCOS RUIZ GONZALEZ <ruizgonzalezmarcos04@gmail.com>
2026-04-28 12:10:42 +0200

    añadida tabal usuraio

--- database/schema.sql
+++ database/schema.sql
@@ -72,3 +72,14 @@ INSERT INTO images_materials (material_id, image_url) VALUES
   (7, 'http://localhost:3000/public/materials/altavozjbl_4.webp'),
 
 
+
+
+-- USUARIOS
+CREATE TABLE IF NOT EXISTS users (
+  id         SERIAL PRIMARY KEY,
+  email      VARCHAR(255) NOT NULL UNIQUE,
+  password   VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_users_email ON users(email);
